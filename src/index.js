08300798f18747f1a07bab9fb7ab9a8af0b155c0
Rebecca86 <kaizens.rebecca@gmail.com>
2025-02-20 16:15:05 +0800

    Changes on the HTML and CSS

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let h2Element = document.querySelector("#ask-h2");
+
+let askAdviceFormElement = document.querySelector("#advice-search-form");
+askAdviceFormElement.addEventListener("submit", generateFinancialAdvisor);
